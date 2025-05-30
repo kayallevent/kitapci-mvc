61010ddcbc9cc8979f51b03f34f70c502a923f36
Levent Kaya <kayalevent003@gmail.com>
2025-05-30 13:33:29 +0300

    hadi bakak

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -2,16 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.Linq;
+using Basics.Models;
+
 
 namespace Basics.Controllers
 {
     public class CartController : Controller
     {
-        public IActionResult Index()
+       public IActionResult Index()
         {
             var cart = HttpContext.Session.GetObjectFromJson<List<int>>("cart") ?? new List<int>();
-            return View(cart);
+            var kitaplar = GetAllBooks(); 
+
+            var sepettekiKitaplar = kitaplar.Where(k => cart.Contains(k.Id)).ToList();
+
+            return View(sepettekiKitaplar);
         }
+
 
         public IActionResult AddToCart(int id)
         {
@@ -19,10 +26,27 @@ namespace Basics.Controllers
             if (!cart.Contains(id))
             {
                 cart.Add(id);
-                HttpContext.Session.SetObjectAsJson("cart", cart);
+                HttpContext.Session.SetObjectAsJson("cart", cart);             
             }
             return RedirectToAction("Index");
         }
+        private List<Kitap> GetAllBooks()
+        {
+            return new List<Kitap>
+            {
+                new Kitap { Id = 1, Ad = "Kürk Mantolu Madonna", Yazar = "Sabahattin Ali", Fiyat = 65.99, ResimUrl = "kitap.jpeg"},
+                new Kitap { Id = 2, Ad = "Çalıkuşu", Yazar = "Reşat Nuri Güntekin", Fiyat = 388.20, ResimUrl = "Çalıkuşu.jpg"},
+                new Kitap { Id = 3, Ad = "Heidi", Yazar = "Johanna Spyri", Fiyat = 59.92, ResimUrl = "Heidi.jpeg"},
+                new Kitap { Id = 3, Ad = "Harry Potter ve Felsefe Taşı", Yazar = "J.K. Rowling", Fiyat = 145.90, ResimUrl = "Harrypot.jpeg"},
+                new Kitap { Id = 3, Ad = "Game Of Thrones Song Of Ice And Fire", Yazar = "George R.R. Martin", Fiyat = 509.99, ResimUrl = "kitap(1).jpeg"},
+                new Kitap { Id = 3, Ad = "Notre-Dame'ın Kamburu", Yazar = "Victor Hugo", Fiyat = 150.00, ResimUrl = "kitap(2).jpeg"},
+                new Kitap { Id = 3, Ad = "Nutuk", Yazar = "Mustafa Kemal Atatürk", Fiyat = 136.05, ResimUrl = "kitap(3).jpeg"},
+                new Kitap { Id = 3, Ad = "Küçük Prens", Yazar = "Antoine De Saint-Exupery", Fiyat = 60, ResimUrl = "Kucukprens.jpg"},
+                new Kitap { Id = 3, Ad = "The Hobbit", Yazar = "J.R.R. Tolkien", Fiyat = 422.24, ResimUrl = "wi_500.jpeg"},
+                new Kitap { Id = 3, Ad = "Don Kişot", Yazar = " Miguel de Cervantes", Fiyat = 128.99, ResimUrl = "wi_800.jpeg" }
+                        
+            };
+        }       
 
         public IActionResult RemoveFromCart(int id)
         {
