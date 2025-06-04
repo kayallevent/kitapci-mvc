fa82bb4468ceed8b533ac3b1cac16f58253db695
Levent Kaya <kayalevent003@gmail.com>
2025-06-04 16:42:45 +0300

    sepetim üste eklendi inş

--- obj/Debug/net6.0/Basics.AssemblyInfo.cs
+++ obj/Debug/net6.0/Basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87108663443b0477e208dfc3f7f2b69767b65ef5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea03ee7c747b32391ad35ba890833a22c6a31e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
